ed79d29ab52cd3d3a6dc7ab04bec183a6898e34b
Petar Maymounkov <petarm@gmail.com>
2020-03-26 15:40:34 -0700

    Add intersection tests: random and read from JSON.

--- trie/intersect_test.go
+++ trie/intersect_test.go
@@ -1,6 +1,7 @@
 package trie
 
 import (
+	"encoding/json"
 	"math/rand"
 	"testing"
 
@@ -9,12 +10,19 @@ import (
 
 func TestIntersectRandom(t *testing.T) {
 	for i := 0; i < 100; i++ {
-		testIntersect(t, randomTestIntersectSample(10, 10, 5))
+		testIntersect(t, randomTestIntersectSample(10, 21, 7))
 	}
 }
 
 func TestIntersect(t *testing.T) {
 	for _, s := range testIntersectSamples {
+		testIntersect(t, s)
+	}
+}
+
+func TestIntersectFromJSON(t *testing.T) {
+	for _, json := range testIntersectJSONSamples {
+		s := testIntersectSampleFromJSON(json)
 		testIntersect(t, s)
 	}
 }
@@ -65,6 +73,14 @@ type testIntersectSample struct {
 	RightKeys []key.Key
 }
 
+func testIntersectSampleFromJSON(srcJSON string) *testIntersectSample {
+	s := &testIntersectSample{}
+	if err := json.Unmarshal([]byte(srcJSON), s); err != nil {
+		panic(err)
+	}
+	return s
+}
+
 var testIntersectSamples = []*testIntersectSample{
 	{
 		LeftKeys:  []key.Key{{1, 2, 3}},
@@ -78,4 +94,25 @@ var testIntersectSamples = []*testIntersectSample{
 		LeftKeys:  []key.Key{{23, 3, 7, 13, 17}},
 		RightKeys: []key.Key{{2, 11, 17, 19, 23}},
 	},
+}
+
+var testIntersectJSONSamples = []string{
+	`
+{
+    "LeftKeys": [
+        "gAlMTjoCy6ZDZFN/0okF65fxscCLVxnQhlJsyfp6uWU=",
+        "IOZLVCi+OWdUqu0N7DX9T6sweK6RffGnBChy3I3G424=",
+        "yIsHasiWkbMESfShoZ5yvS4fv6m0KnBSV6emNyMgYrg=",
+        "qAISv6yZjs3WWlDC89iJUSdq45F0D/1y9fLnsPvavdA=",
+        "aOl3hzLR2jArVpbEWaLOjH4QqoUo/7pJlmbY1mCHgYI=",
+        "6P28fa97aZOOImD9TGO+x2+XJbzPSEh5rP6NqKoDj88=",
+        "2Lh4bHSBoSvqloX+v7IYMvmq0k0bUcYAkQesOUPxLVo=",
+        "eLqLdLjusZJlqDZT94U0PC4bdVQeUNaGwkOn9i5KRoI="
+    ],
+    "RightKeys": [
+        "gAlMTjoCy6ZDZFN/0okF65fxscCLVxnQhlJsyfp6uWU=",
+        "qAISv6yZjs3WWlDC89iJUSdq45F0D/1y9fLnsPvavdA="
+    ]
+}
+	`,
 }
