4e7399a3f99555a7db0be9c532d179eedb1f9fa2
Ondrej Prazak <o.prazak5@seznam.cz>
2021-08-10 08:06:53 +0200

    Refs #5 - Test tries are not reusing nodes

--- trie/union_test.go
+++ trie/union_test.go
@@ -26,13 +26,34 @@ func TestUnionFromJSON(t *testing.T) {
 func testUnion(t *testing.T, sample *testSetSample) {
 	left := FromKeys(sample.LeftKeys)
 	right := FromKeys(sample.RightKeys)
-
+	trie := Union(left, right)
 	expected := FromKeys(UnionKeySlices(sample.LeftKeys, sample.RightKeys))
-	actual := Union(left, right)
-	if d := actual.CheckInvariant(); d != nil {
-		t.Fatalf("trie invariant discrepancy: %v", d)
+
+	if !Equal(trie, expected) {
+		t.Errorf("union does not have expected values")
 	}
-	if !Equal(expected, actual) {
-		t.Errorf("union of %v\n and %v\n expected %v\n got %v", left, right, expected, actual)
+	nodesMap := trieNodes(left, make(map[*Trie]bool))
+	nodesMap = trieNodes(right, nodesMap)
+	testTrieNotSameReference(t, nodesMap, trie)
+}
+
+func testTrieNotSameReference(t *testing.T, nodesMap map[*Trie]bool, union *Trie) {
+	if union == nil {
+		return
 	}
+	if nodesMap[union] {
+		t.Errorf("Found a reference to original node in union: %v\n", union)
+	}
+	testTrieNotSameReference(t, nodesMap, union.Branch[0])
+	testTrieNotSameReference(t, nodesMap, union.Branch[1])
+}
+
+func trieNodes(trie *Trie, nodesMap map[*Trie]bool) map[*Trie]bool {
+	if trie == nil {
+		return nodesMap
+	}
+	nodesMap[trie] = true
+	nodesMap = trieNodes(trie.Branch[0], nodesMap)
+	nodesMap = trieNodes(trie.Branch[1], nodesMap)
+	return nodesMap
 }
